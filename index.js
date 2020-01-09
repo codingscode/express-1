50d5966e29abd228fdf99d73a47f55786225d1ac
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-09 17:03:42 -0300

    parte 3

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-    res.send('Estou <b>bem</b>!')
+app.get('/opa',(req, res) => {
+    //res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+    /*res.json({
+        nome: 'celular',
+        preco: 600.00,
+        desconto: 0.12 
+    }) */
+    /*res.json([
+        {id: 7, nome: 'Ana', posicao: 1},
+        {id: 34, nome: 'Bia', posicao: 2},
+        {id: 73, nome: 'Carlos', posicao: 3}
+    ])*/
+    res.json({
+        data: [
+            {id: 7, nome: 'Ana', posicao: 1},
+            {id: 34, nome: 'Bia', posicao: 2},
+            {id: 73, nome: 'Carlos', posicao: 3}
+          ],
+        contar: 30,
+        skip: 0,
+        limite: 3,
+        status: 200
+    })
 })
 
 app.listen(3000, () => {
