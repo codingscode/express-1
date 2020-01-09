f6ffc9855fb147fdbb361827cdbefc58d9429158
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-09 16:30:56 -0300

    parte 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res) => {
+    res.send('Estou <b>bem</b>!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
 
+//localhost:3000
