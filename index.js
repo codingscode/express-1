aa5315a7312725121d72b01f1088e806627530bb
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-10 12:56:39 -0300

    parte 7

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
 
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(saudacao('Roberto'))
 
 app.use((req, res, proximo) => {  //sem o 3º parametro proximo chama o próximo app
@@ -15,14 +20,15 @@ app.get('/clientes/relatorio', (req, res) => {
 })
 
 app.post('/corpo', (req, res) => {
-    let corpo = ''
+    /*let corpo = ''
     req.on('data', function(parte) {
       corpo += parte
     })
   
     req.on('end', function() {
       res.send(corpo)
-    })
+    })*/
+    res.send(req.body)
 })
 
 app.get('/clientes/:id', (req, res) => {
