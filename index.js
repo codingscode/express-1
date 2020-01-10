d6a829e8e91192e96e292210dcfac51403460a90
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-09 22:57:22 -0300

    parte 4

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/opa',(req, res) => {
+app.use((req, res, proximo) => {  //sem o 3º parametro proximo chama o próximo app
+    console.log('Antes...')  //fazer requisicao no browser ou outro
+    proximo()
+})
+
+
+app.get('/opa',(req, res, proximo) => {
     //res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
     /*res.json({
         nome: 'celular',
@@ -13,6 +19,7 @@ app.get('/opa',(req, res) => {
         {id: 34, nome: 'Bia', posicao: 2},
         {id: 73, nome: 'Carlos', posicao: 3}
     ])*/
+    console.log('Durante...')  
     res.json({
         data: [
             {id: 7, nome: 'Ana', posicao: 1},
@@ -24,10 +31,17 @@ app.get('/opa',(req, res) => {
         limite: 3,
         status: 200
     })
+    proximo()
 })
+
+app.use((req, res) => {  
+    console.log('Depois...')  
+})
+
 
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
 
 //localhost:3000
+//localhost:3000/opa
